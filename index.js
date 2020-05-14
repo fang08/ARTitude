64553501d11f354ad9aee0fa3fcf4e993b404bca
Fang Yao <fangyao0818@gmail.com>
2020-05-14 15:36:43 -0700

    add log in and register pages with authentication

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+
 const Campground = require("./models/campground");
 const Comment = require("./models/comment");
+const User = require("./models/user");
 const seedDB = require("./seed");
+
 const port = 3000;
 
 mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true, useUnifiedTopology: true});
@@ -13,12 +18,32 @@ app.set("view engine", "ejs");
 
 seedDB();
 
-// routes
+// passport config
+app.use(require("express-session")({
+  secret: "THISISARANDOMSECRET",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+// ==============
+// Routes
+// ==============
+
 app.get("/", (req, res) => {
   res.render("landing");
 });
 
-// INDEX
+// INDEX of campgrounds
 app.get("/campgrounds", (req, res) => {
   Campground.find({}, (err, allCampgrounds) => {
     if(err) {
@@ -63,8 +88,9 @@ app.get("/campgrounds/:id", (req, res) => {
   });
 });
 
+
 // NEW for comments
-app.get("/campgrounds/:id/comments/new", (req, res) => {
+app.get("/campgrounds/:id/comments/new", isLoggedIn, (req, res) => {
   Campground.findById(req.params.id, (err, campground) => {
     if(err) {
       console.log(err);
@@ -76,7 +102,7 @@ app.get("/campgrounds/:id/comments/new", (req, res) => {
 });
 
 // CREATE new comment
-app.post("/campgrounds/:id/comments", (req, res) => {
+app.post("/campgrounds/:id/comments", isLoggedIn, (req, res) => {
   Campground.findById(req.params.id, (err, campground) => {
     if(err) {
       console.log(err);
@@ -96,5 +122,51 @@ app.post("/campgrounds/:id/comments", (req, res) => {
     }
   });
 });
+
+
+// NEW register form
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+// CREATE new user
+app.post("/register", (req, res) => {
+  let newUser = new User({username: req.body.username});  // only user name, not password
+  User.register(newUser, req.body.password, (err, user) => {
+    if(err) {
+      console.log(err);
+      return res.render("register");
+    }
+    passport.authenticate("local")(req, res, () => {
+      res.redirect("/campgrounds");
+    });
+  });
+});
+
+// SHOW login form
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+// handle login
+app.post("/login", 
+  passport.authenticate("local", 
+  { successRedirect: "/campgrounds", failureRedirect: "/login"}),
+  (req, res) => {
+});
+
+// logout
+app.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+
+// middleware, check whether a user has logged in or not
+function isLoggedIn(req, res, next) {
+  if(req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
