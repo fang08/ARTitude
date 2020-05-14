702f0dbb77bcf49edf2888170288a748ca960a3d
Fang Yao <fangyao0818@gmail.com>
2020-05-14 13:54:34 -0700

    install eslint and node plugin

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:node/recommended"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 11
+    },
+    "rules": {
+        "node/exports-style": ["error", "module.exports"],
+    }
+};
